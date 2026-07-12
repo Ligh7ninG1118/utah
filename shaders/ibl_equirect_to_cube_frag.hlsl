5a1e8faae26ceb68dda6472c9ff340239e0ea6ab
Ligh7ninG <Lt1118@live.cn>
2026-07-12 07:26:36 -0700

    fixed brdf lut sampling bug

--- shaders/ibl_equirect_to_cube_frag.hlsl
+++ shaders/ibl_equirect_to_cube_frag.hlsl
@@ -29,7 +29,7 @@ float2 SampleSphericalMap(float3 v)
 float4 main(PSInput input) : SV_TARGET
 {
     float2 uv = SampleSphericalMap(normalize(input.localPos));
-    float3 color = textures[pc.equirectIndex].Sample(textureSamplers[pc.samplerIndex], uv).rgb;
+    float3 color = textures[pc.equirectIndex].SampleLevel(textureSamplers[pc.samplerIndex], uv, 0.0f).rgb;
     
     return float4(color, 1.0f);
 }
